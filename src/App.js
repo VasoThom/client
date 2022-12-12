04309d8b6988906d1f4687eefe802fefb86db024
Vaso <vasothom@hotmail.com>
2022-12-12 16:12:03 +0100

    backend und frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [backenddata, setBackendData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {typeof backenddata.users === "undefined" ? (
+        <p>not found </p>
+      ) : (
+        backenddata.users.map((user, i) => <p key={i}> {user}</p>)
+      )}
     </div>
   );
 }
